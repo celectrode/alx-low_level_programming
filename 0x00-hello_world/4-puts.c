daf57535cc9c63114c659e6befbbc3e8a57d1fa7
celectrode <izzywapmoh@gmail.com>
2022-06-03 05:58:20 -0700

    My commit for task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - A C program that prints character value
  * return: 0
 */
-int main (void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
